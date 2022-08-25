f57f33be824454da2496566d8b2632c27b506c72
lducoding <lducoding@gmail.com>
2022-08-25 22:39:22 +0900

    설명 추가

--- java-to-kotlin-basic/src/main/kotlin/com/prac/lec10/Animal.kt
+++ java-to-kotlin-basic/src/main/kotlin/com/prac/lec10/Animal.kt
@@ -3,6 +3,7 @@ package com.prac.lec10
 abstract class Animal(
     protected val species: String,
     // 프로퍼티에 대한 오버라이드를 할 때는 open을 붙여야 한다.
+    // 기본적으로 final 이 기본값으로 있어서 그렇다.
     protected open val legCount: Int
 ) {
 
--- java-to-kotlin-basic/src/main/kotlin/com/prac/lec10/Base2.kt
+++ java-to-kotlin-basic/src/main/kotlin/com/prac/lec10/Base2.kt
@@ -3,7 +3,7 @@ package com.prac.lec10
 fun main() {
     val test = Derived2()
 }
-
+// 상위클래스 설계할 때 생성자나 초기화 블록에 사용되는 프로퍼티에는 open을 피해야 한다.
 open class Base2{
     open val number: Int
         get() {
